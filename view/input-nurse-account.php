841b42bc15b193977482cd69f3231355d8c9f745
jomarcuetara <88090928+jomarcuetara@users.noreply.github.com>
2022-05-29 19:04:29 +0800

    new nurse account form

---
+++ view/input-nurse-account.php
@@ -0,0 +1,29 @@
+<?php 
+	include "../templates/header.php";
+
+ ?>
+
+<body>
+<h1>New Nurse Account</h1>
+<form action="../scripts/process-nurse-account.php" method="POST">
+            
+            <label for="" class="lbl">Nurse ID: </label>
+            <input type="text" id="#" name="#" ><br>
+            <label for="" class="lbl">First Name: </label>
+            <input type="text" id="#" name="#"><br>
+            <label for="" class="lbl">Last Name: </label>
+            <input type="text" class=""><br>
+            <label for="" class="lbl">Age: </label>
+            <input type="text" class=""><br>
+            <label for="" class="lbl">Gender: </label>
+            <input type="text" class=""><br>
+            <label for="" class="lbl">Address: </label>
+            <input type="text" class=""><br>
+            <label for="" class="lbl">Contact Number: </label>
+            <input type="text" class=""><br>
+            
+            <input type="back" value="Back">
+            <input type="create account" value="Create Account">       
+</form> 
+</body>
+
