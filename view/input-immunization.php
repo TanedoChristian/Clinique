fc69016aa1ce6d48f871a0efa774fc95abdfeca0
MY PC <hiwazero>
2022-05-29 18:26:51 +0800

    ADD INPUT IMMUNE FORM

--- view/input-immunization.php
+++ view/input-immunization.php
@@ -15,7 +15,18 @@
 </head>
 <body>
 
-<h1> test </h1>
+<form action="../scripts/process-immune.php" method="POST">
+            
+            <label for="" class="lbl">Vaccine Name: </label>
+            <input type="text" id="#" name="#" ><br>
+            <label for="" class="lbl">Student ID: </label>
+            <input type="text" id="#" name="#"><br>
+            <label for="" class="lbl">Parent's Consent: </label>
+            <input type="file" class=""><br>
+            
+            <input type="submit" value="Submit">
+            <input type="reset" value="Cancel">       
+</form>
     
 </body>
 </html>
