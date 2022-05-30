10cf1e4aff11dfd322abe085dad2012bf8b4faa9
MY PC <hiwazero>
2022-05-30 19:54:59 +0800

    latest mamac

--- view/appointment.php
+++ view/appointment.php
@@ -6,14 +6,14 @@
 <body>
     <h1>Book Appointment</h1>
     <form action="../scripts/process-appointment.php" method="POST">
-            
             <label for="" class="lbl">Pick Date: </label>
             <input type="date" id="#" name="#" ><br>
             <label for="" class="lbl">Pick Time: </label>
             <input type="text" id="#" name="#"><br>
            
-            
             <input type="submit" value="Submit" name="submit">
-            <input type="reset" value="Cancel">       
+            <input type="reset" value="Cancel">
+            
+            
     </form>
 </body>
